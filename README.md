6ff4cd626a202741581ba6187072ac292bca960c
SincerelyBrittany <sincerelybr@gmail.com>
2020-07-28 14:09:20 -0400

    updates readme and practing string functions in app.py

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # pypractice
+
+Reviews python using the freecodecamp 4 hour python review:
+https://www.youtube.com/watch?v=rfscVS0vtbw
--- app.py
+++ app.py
@@ -10,9 +10,53 @@ print("  / |")
 print(" /  |")
 print("/___|")
 
-# variables
+# variables - use underscore
+character_name = "John"
+character_age = "75"
+integer = 75
+isBoolean = True
+floats = 50.040395
 
-print("There once was a man named George")
-print("He was 70 years old")
-print("He really liked the name George")
-print("but he did not ike being 70.")
+print("There once was a man named " + character_name + ",")
+print("He was " + character_age + " years old")
+
+#Updates the name
+character_name = "Tom"
+character_age = "50"
+print("He really liked the name " + character_name)
+print("but he did not ike being "+ character_age +".")
+
+#working with strings in py
+
+#inserts a new line
+print("Sincerely\nBrittany")
+#inserts a quotation
+print("Sincerely\"Brittany")
+#inserts a backslash
+print("Sincerely\Brittany")
+
+phrase = "Sincerely Brittany"
+print(phrase)
+
+#concatenate
+print(phrase + "is awesome")
+
+
+#functions
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+
+#length function
+print(len(phrase))
+
+#get individual characters ina string
+print(phrase[2])
+
+#passing a parameter
+print(phrase.index("S")) #where the first "S" index is located
+print(phrase.index("i"))
+print(phrase.index("Brit"))
+
+print(phrase.replace("Brittany", "Britt"))
