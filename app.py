a466e4b9de05a6b2c4336685799b007db6f9e2e5
SincerelyBrittany <sincerelybr@gmail.com>
2020-07-28 13:34:40 -0400

    first commit - adds app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+#first hello world
+
+print("Hello World")
+
+# first shape
+
+print("My first shape is drawing a shape")
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+
+# variables
+
+print("There once was a man named George")
+print("He was 70 years old")
+print("He really liked the name George")
+print("but he did not ike being 70.")
